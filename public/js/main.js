ce6e6e0fbd8658302bbcf235a0139f0b5bfed165
YessiGarcia <aria.801900@gmail.com>
2017-07-18 22:23:37 -0500

    intento de validar

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-      $('.carousel').carousel();
+      $('.carousel.carousel-slider').carousel({fullWidth: true});
     });
 
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+var numero = $("#cel").value;
+var terminos = $("#terminos");
+var registro = $("#registro");
 
+if (numero.length == 10 & terminos.checked == "checked" ){
+	registro.removeClass("disabled");
+}
